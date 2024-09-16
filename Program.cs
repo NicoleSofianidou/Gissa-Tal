bc321dc037ac6dfd839c7270044d7017ae15a47c
NicoleSofianidou <niso07001@utb.vaxjo.se>
2024-09-16 10:12:34 +0200

    Här är ett gissningsspel

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Random random= new Random();
+int randomNumber = random.Next(0,101);
+int antalgissningar= 0;
+Console.WriteLine("Gissa vilket tal jag tänker på");
+int svar1= int.Parse(Console.ReadLine());
+
+while (svar1 != randomNumber)
+{
+    antalgissningar++;
+    if (svar1>randomNumber)
+    {
+        Console.WriteLine("Din gissning är för hög, gissa igen");
+    }
+    else if (svar1<randomNumber)
+    {
+        Console.WriteLine("Din gissning är för låg, gissa igen");
+
+    }
+    svar1 = int.Parse(Console.ReadLine());
+   
+}
+antalgissningar++;
+Console.WriteLine("Grattis du har gissat rätt!");
+Console.WriteLine($" Du gissade {antalgissningar} gånger");
+
